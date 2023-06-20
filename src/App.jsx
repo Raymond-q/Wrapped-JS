ff9a876ed7af4a2131a7a649cd87e230c505280b
Raymond Qian <raymondq@uchicago.edu>
2023-06-19 20:41:35 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import './App.css'
+
+function api(){
+  
+}
+
+function click(count, setCount, started, setStarted, time){
+  if (!started){
+    setStarted(true)
+    setTimeout(endTime(setStarted, count, setCount, time), time)
+  }
+  setCount(count + 1);
+}
+
+function reset(setCount){
+  setCount(0);
+}
+
+function endTime(setStarted, count, setCount, time){
+  setStarted(false)
+  let temp = count
+  setCount(0)
+  console.log(temp * 1000 / time)
+}
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [started, setStarted] = useState(false)
+  
+  return (
+    <div className="App">
+      <div className="card">
+        <button onClick={() => click(count, setCount, started, setStarted, 1000)}>
+          count is {count}
+        </button>
+      </div>
+      <div className="card">
+        <button onClick={() => reset(setCount)}>
+          reset count
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
