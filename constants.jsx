9e6ed42c4648d94bfa675694c3132b4a267b9c07
Raymond-q <raycheetah14@yahoo.com>
2023-06-19 21:55:18 -0700

    test

--- constants.jsx
+++ constants.jsx
@@ -1,3 +1,3 @@
-export const client_id = "client id";
-export const client_secret = "client secret";
+export const client_id = "455925192b08491f87366c9a6a7e4b1e";
+export const client_secret = "5692d75a70724843ac0575e3c020733f";
 export const redirect_uri = "http://127.0.0.1:5173/";
